5e861a2d79b818b1ac76902e5019799adf85f243
Anastasia Golub <av_golub@mail.ru>
2024-06-10 11:30:17 +0300

    Add files via upload

---
+++ Homework7.py
@@ -0,0 +1,20 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+a = sorted(students)
+
+b = sum(grades[0])/grades[0].__len__()
+с = sum(grades[1])/grades[1].__len__()
+d = sum(grades[2])/grades[2].__len__()
+e = sum(grades[3])/grades[3].__len__()
+f = sum(grades[4])/grades[4].__len__()
+
+print(
+    a[0],":", b,"," ,
+    a[1],":", с,"," ,
+    a[2], ":", d,"," ,
+    a[3], ":", e,"," ,
+    a[4], ":", f,"."
+)
+
+
+
